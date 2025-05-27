d66cf37fede7ce181105983dc4934229c89de706
HLN177 <harryleen177@gmail.com>
2025-05-27 02:30:33 +0100

    feat(reader): readRaw, readString, readNumber

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu(
+  {
+    typescript: true
+  },
+  {
+    rules: {
+      'curly': ['error', 'all'],
+      'no-console': 'off',
+      'dot-notation': 'off',
+      'style/object-curly-newline': ['error', {
+        ObjectExpression: {
+          multiline: true,
+          minProperties: 1
+        },
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3
+        }
+      }],
+      'style/indent': ['error', 2],
+      'style/quotes': ['error', 'single'],
+      'style/semi': ['error', 'always'],
+      'style/brace-style': ['error', '1tbs'],
+      'style/comma-dangle': ['error', 'never'],
+      'style/arrow-parens': ['error', 'as-needed'],
+      'style/eol-last': ['error', 'always'],
+      'test/prefer-lowercase-title': ['error', {
+        ignore: ['describe']
+      }],
+      'ts/no-namespace': ['error', {
+        allowDeclarations: true
+      }],
+      'ts/consistent-type-definitions': 'off'
+    }
+  }
+);
