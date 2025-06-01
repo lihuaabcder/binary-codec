9f4b10306eaf7c86541dacb478c56cee7db261ed
HLN177 <harryleen177@gmail.com>
2025-06-01 21:00:42 +0100

    feat: read bitmask

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,6 @@
-export type NumberType =
-  | 'uint8'
-  | 'int8'
-  | 'uint16'
-  | 'int16'
-  | 'uint32'
-  | 'int32'
-  | 'float32'
-  | 'float64';
+export type NumberType = 'uint' | 'int' | 'float';
+
+export type NumberByteLength = 1 | 2 | 4;
 
 export type MetaField = {
   name: string
@@ -18,15 +12,49 @@ export type RawField = MetaField & {
   type: 'raw'
 };
 
-export type NumberField = MetaField & {
-  type: 'number'
+export type NumberField = Omit<MetaField, 'byteLength'> & {
+  type: NumberType
+  byteLength: NumberByteLength
 };
 
 export type StringField = MetaField & {
   type: 'string'
 };
 
-export type Field = RawField | NumberField | StringField;
+export type BitmaskField = MetaField & {
+  type: 'bitmask'
+  map: BitmaskMap
+};
+
+export type BitmaskMap = Record<string, BitField>;
+
+export type BitField = BooleanBitField | UintBitField | EnumBitField;
+
+export type BitPosition = number;
+
+export type BitRange = [start: number, end: number];
+
+export type BooleanBitField = {
+  bits: BitPosition
+  type: 'boolean'
+};
+
+export type UintBitField = {
+  bits: BitPosition | BitRange
+  type: 'uint'
+};
+
+export interface EnumBitField {
+  bits: BitPosition | BitRange
+  type: 'enum'
+  values: string[]
+}
+
+export type Field =
+  | RawField
+  | NumberField
+  | StringField
+  | BitmaskField;
 
 export type CodecSpec = {
   byteLength: number
