944750993b69ff813a1ce55280fe4f1dc6c819f0
HLN177 <harryleen177@gmail.com>
2025-08-16 21:07:28 +0100

    feat: bitset write

--- src/codecs/bitset.ts
+++ src/codecs/bitset.ts
@@ -4,7 +4,7 @@ export type BitsetField = MetaField<'bitset'>;
 
 export const bitsetCodec: Codec<BitsetField, boolean[]> = {
   type: 'bitset',
-  read: (view, spec) => {
+  read: (view, spec, ctx) => {
     const { byteOffset, byteLength } = spec;
     const result: boolean[] = [];
     const total = byteLength * 8;
@@ -12,11 +12,67 @@ export const bitsetCodec: Codec<BitsetField, boolean[]> = {
     for (let i = 0; i < total; i++) {
       const byteIndex = Math.floor(i / 8);
       const bitIndex = i % 8;
-      const byte = view.getUint8(byteOffset + byteIndex);
+      const byte = ctx.get('number').read(
+        view,
+        {
+          numberType: 'uint',
+          byteOffset: byteOffset + byteIndex,
+          byteLength: 1
+        },
+        ctx
+      );
       const bit = (byte >> bitIndex) & 1;
       result.push(Boolean(bit));
     }
 
     return result;
+  },
+  write: (view, spec, value, ctx) => {
+    const { byteOffset, byteLength } = spec;
+
+    const totalBits = byteLength * 8;
+
+    // set all bits to 0
+    for (let i = 0; i < byteLength; i++) {
+      ctx.get('number').write!(
+        view,
+        {
+          numberType: 'uint',
+          byteOffset: byteOffset + i,
+          byteLength: 1
+        },
+        0,
+        ctx
+      );
+    }
+
+    for (let i = 0; i < totalBits; i++) {
+      if (!value[i]) {
+        continue;
+      }
+
+      const byteIndex = Math.floor(i / 8);
+      const bitIndex = i % 8;
+      const pre = ctx.get('number').read(
+        view,
+        {
+          numberType: 'uint',
+          byteOffset: byteOffset + byteIndex,
+          byteLength: 1
+        },
+        ctx
+      );
+
+      ctx.get('number').write!(
+        view,
+        {
+          numberType: 'uint',
+          byteOffset: byteOffset + byteIndex,
+          byteLength: 1
+        },
+        pre | (1 << bitIndex),
+        ctx
+      );
+    }
   }
 };
