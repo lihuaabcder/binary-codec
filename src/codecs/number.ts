7592bb016cb4da99ab264b7bc07d24aa226b3b35
HLN177 <harryleen177@gmail.com>
2025-08-16 17:26:03 +0100

    feat: number write

--- src/codecs/number.ts
+++ src/codecs/number.ts
@@ -10,14 +10,35 @@ export type NumberField = Omit<MetaField<'number'>, 'byteLength'> & {
 };
 
 // TODO bigint..
-const typeToDataViewMethod: Record<string, keyof DataView> = {
-  uint8: 'getUint8',
-  int8: 'getInt8',
-  uint16: 'getUint16',
-  int16: 'getInt16',
-  uint32: 'getUint32',
-  int32: 'getInt32',
-  float32: 'getFloat32'
+const typeToDataViewMethod: Record<string, { get: keyof DataView, set: keyof DataView }> = {
+  uint8: {
+    get: 'getUint8',
+    set: 'setUint8'
+  },
+  int8: {
+    get: 'getInt8',
+    set: 'setInt8'
+  },
+  uint16: {
+    get: 'getUint16',
+    set: 'setUint16'
+  },
+  int16: {
+    get: 'getInt16',
+    set: 'setInt16'
+  },
+  uint32: {
+    get: 'getUint32',
+    set: 'setUint32'
+  },
+  int32: {
+    get: 'getInt32',
+    set: 'setInt32'
+  },
+  float32: {
+    get: 'getFloat32',
+    set: 'setFloat32'
+  }
   // float64: 'getFloat64'
 };
 
@@ -33,7 +54,7 @@ export const numberCodec: Codec<NumberField, number> = {
 
     const readerType = `${numberType}${byteLength * 8}`;
 
-    const method = typeToDataViewMethod[readerType];
+    const method = typeToDataViewMethod[readerType]['get'];
 
     if (!method) {
       throw new Error(`Unsupported number type: ${numberType}`);
@@ -44,5 +65,27 @@ export const numberCodec: Codec<NumberField, number> = {
     const fn = view[method] as (...args: any[]) => number;
 
     return isSingleByte ? fn.call(view, byteOffset) : fn.call(view, byteOffset, littleEndian);
+  },
+  write: (view, spec, value) => {
+    const {
+      numberType,
+      byteOffset,
+      byteLength,
+      littleEndian = false
+    } = spec;
+
+    const readerType = `${numberType}${byteLength * 8}`;
+
+    const method = typeToDataViewMethod[readerType]['set'];
+
+    if (!method) {
+      throw new Error(`Unsupported number type: ${numberType}`);
+    }
+
+    const isSingleByte = byteLength === 1;
+
+    const fn = view[method] as (off: number, val: number, le?: boolean) => void;
+
+    isSingleByte ? fn.call(view, byteOffset, value) : fn.call(view, byteOffset, value, littleEndian);
   }
 };
--- test/codecs/number.test.ts
+++ test/codecs/number.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { numberCodec } from '../../src/codecs/number';
-import { dummyCtx, toView } from '../helper';
+import { dummyCtx, toPlainView, toView, viewToArray } from '../helper';
 
 describe('number.read', () => {
   it('should read uint8 correctly', () => {
@@ -88,4 +88,155 @@ describe('number.read', () => {
     );
     expect(fn).toThrow();
   });
+});
+
+describe('number.write', () => {
+  it('should write uint8 correctly', () => {
+    const view = toPlainView(1);
+    const value = 255;
+
+    numberCodec.write!(
+      view,
+      {
+        numberType: 'uint',
+        byteOffset: 0,
+        byteLength: 1
+      },
+      value,
+      dummyCtx
+    );
+
+    expect(viewToArray(view)).toEqual([0xFF]);
+  });
+
+  it('should write int8 correctly (negative)', () => {
+    const view = toPlainView(1);
+    const value = -1;
+
+    numberCodec.write!(
+      view,
+      {
+        numberType: 'int',
+        byteOffset: 0,
+        byteLength: 1
+      },
+      value,
+      dummyCtx
+    );
+
+    expect(viewToArray(view)).toEqual([0xFF]);
+  });
+
+  it('should write uint16 little-endian', () => {
+    const view = toPlainView(2);
+    const value = 0x1234;
+
+    numberCodec.write!(
+      view,
+      {
+        numberType: 'uint',
+        byteOffset: 0,
+        byteLength: 2,
+        littleEndian: true
+      },
+      value,
+      dummyCtx
+    );
+
+    expect(viewToArray(view)).toEqual([0x34, 0x12]);
+  });
+
+  it('should write uint16 big-endian (default)', () => {
+    const view = toPlainView(2);
+    const value = 0x1234;
+
+    numberCodec.write!(
+      view,
+      {
+        numberType: 'uint',
+        byteOffset: 0,
+        byteLength: 2,
+      },
+      value,
+      dummyCtx
+    );
+
+    expect(viewToArray(view)).toEqual([0x12, 0x34]);
+  });
+
+  it('should write float32 corrently', () => {
+    const view = toPlainView(4);
+    const value = 10.0;
+
+    numberCodec.write!(
+      view,
+      {
+        numberType: 'float',
+        byteOffset: 0,
+        byteLength: 4,
+        littleEndian: true
+      },
+      value,
+      dummyCtx
+    );
+
+    expect(viewToArray(view)).toEqual([0x00, 0x00, 0x20, 0x41]);
+  });
+
+  it('should respect non-zero byteOffset', () => {
+    const view = toPlainView(10);
+    const value = 10.0;
+
+    numberCodec.write!(
+      view,
+      {
+        numberType: 'float',
+        byteOffset: 6,
+        byteLength: 4,
+        littleEndian: true
+      },
+      value,
+      dummyCtx
+    );
+
+    expect(viewToArray(view)).toEqual([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x41]);
+  });
+
+  it('should throw on unsupported type', () => {
+    const view = toPlainView(1);
+    const value = 255;
+
+    const fn = () => numberCodec.write!(
+      view,
+      {
+        numberType: 'uint',
+        byteOffset: 0,
+        byteLength: 3 as any
+      },
+      value,
+      dummyCtx
+    );
+
+    expect(fn).toThrow();
+  });
+
+  it('should throw when value out of range', () => {
+    // todo: ideally have a optional validate fn in codec
+
+    // const view = toPlainView(1);
+    // const value = 256;
+
+    // const fn = () => numberCodec.write!(
+    //   view,
+    //   {
+    //     numberType: 'uint',
+    //     byteOffset: 0,
+    //     byteLength: 1
+    //   },
+    //   value,
+    //   dummyCtx
+    // );
+
+    // expect(fn).toThrow();
+  })
 });
