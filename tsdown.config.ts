6fb0b6a7298cd229d2923945d5d93a8a8995a047
HLN177 <harryleen177@gmail.com>
2025-08-18 16:27:28 +0100

    feat: tsdown bundling

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import type { UserConfig } from 'tsdown';
+import { defineConfig } from 'tsdown';
+
+const config: UserConfig = defineConfig({
+  entry: 'src/index.ts',
+  dts: {
+    sourcemap: true
+  }
+});
+
+export default config;
