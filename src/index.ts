5e215ca7b7e0914aafabf7d1172a7dfe2e400691
HLN177 <harryleen177@gmail.com>
2025-08-22 03:45:26 +0100

    feat: export validation function and type

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './entries/index.ts';
 export * from './registry/default.ts';
 export * from './registry/registry.ts';
 export * from './types.ts';
+export * from './validation/types.ts';
+export * from './validation/validator.ts';
